25e182c7ee3fb6b7815c393cf823a6ddd14c9845
cbrgm <chris@cbrgm.net>
2022-11-29 23:27:52 +0100

    add go generate comment

--- navigation/navigation.go
+++ navigation/navigation.go
@@ -1,5 +1,6 @@
 package navigation
 
+// go:generate mockery --dir navigation --name Navigation --output navigation/mocks --outpkg mocks --with-expecter
 type Navigation interface {
 	IsOffline() bool // whether to use offline navigation or not
 }
